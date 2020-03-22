6cbdf635ea7aedfe6301dd2170faebaa562f8405
Kanad Gupta <kgupta@umn.edu>
2020-03-22 13:55:13 -0500

    Add better error messages

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ async function run() {
   try {
     const projectId = core.getInput('project-id');
     const authorization = core.getInput('auth-token');
+    if (!projectId || !authorization) {
+      core.setFailed(
+        'Oops! Project ID and Auth Token are required. See https://github.com/kanadgupta/glitch-sync#inputs for details.'
+      );
+      return;
+    }
     const { owner, repo } = github.context.repo;
     const repoQs = querystring.stringify({ projectId, repo: `${owner}/${repo}` });
     const url = `https://api.glitch.com/project/githubImport?${repoQs}`;
