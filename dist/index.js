b6fa5fc1e9d5c73d3026e8f0316eb5e09d59bc3c
Kanad Gupta <kgupta@umn.edu>
2020-12-21 11:13:27 -0600

    chore: update dist

--- dist/index.js
+++ dist/index.js
@@ -22,6 +22,7 @@ async function run() {
   try {
     const projectId = core.getInput('project-id');
     const authorization = core.getInput('auth-token');
+    const path = core.getInput('path');
     if (!projectId || !authorization) {
       core.setFailed(
         'Oops! Project ID and Auth Token are required. See https://github.com/kanadgupta/glitch-sync#inputs for details.'
@@ -29,7 +30,9 @@ async function run() {
       return;
     }
     const { owner, repo } = github.context.repo;
-    const repoQs = querystring.stringify({ projectId, repo: `${owner}/${repo}` });
+    const query = { projectId, repo: `${owner}/${repo}` };
+    if (path) query.path = path;
+    const repoQs = querystring.stringify(query);
     const url = `https://api.glitch.com/project/githubImport?${repoQs}`;
     const post = bent(url, 'POST', { authorization });
     const resp = await post();
