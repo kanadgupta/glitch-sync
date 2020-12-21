426ce244e07ce25aae3cb60ee6f5e39be5a062bb
Kanad Gupta <kgupta@umn.edu>
2020-12-21 11:53:16 -0600

    chore: update dist (duh)

--- dist/index.js
+++ dist/index.js
@@ -29,15 +29,20 @@ async function run() {
       );
       return;
     }
+    core.setSecret(projectId);
+    core.setSecret(authorization);
     const { owner, repo } = github.context.repo;
     const query = { projectId, repo: `${owner}/${repo}` };
     if (path) query.path = path;
     const repoQs = querystring.stringify(query);
+    core.debug('query string:', repoQs);
     const url = `https://api.glitch.com/project/githubImport?${repoQs}`;
     const post = bent(url, 'POST', { authorization });
     const resp = await post();
+    core.debug(resp);
     core.setOutput('response', resp.statusMessage);
   } catch (error) {
+    core.debug(error);
     if (error.responseBody) {
       // If error hitting Glitch API, send raw error response body to debug logs
       // Docs: https://github.com/actions/toolkit/blob/main/docs/action-debugging.md#step-debug-logs
