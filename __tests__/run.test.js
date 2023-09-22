fdcb03a1e7f42b0ba2289c6dfb99025d85fe82cf
Kanad Gupta <git@kanad.dev>
2023-09-22 18:47:58 -0500

    test: another test case

--- __tests__/run.test.js
+++ __tests__/run.test.js
@@ -120,6 +120,25 @@ describe('glitch-sync main runner tests', () => {
     expect(output).toContain('::error::Error syncing to Glitch: Forbidden');
   });
 
+  it('should fail and display status text if Glitch API responds with non-JSON response and custom status text', async () => {
+    vi.stubEnv('INPUT_AUTH-TOKEN', authorization);
+    vi.stubEnv('INPUT_PROJECT-ID', projectId);
+
+    server.use(
+      rest.post(glitchUrl, (req, res, ctx) => {
+        validateReq(req);
+        return res(ctx.status(403, 'custom status text'), ctx.text('<html></html>'));
+      }),
+    );
+
+    await expect(run()).resolves.toBeUndefined();
+
+    const output = getCommandOutput();
+
+    expect(output).toContain('::debug::Raw error response from Glitch: <html></html>');
+    expect(output).toContain('::error::Error syncing to Glitch: custom status text');
+  });
+
   it('should fail if Glitch API fails with JSON response body', async () => {
     vi.stubEnv('INPUT_AUTH-TOKEN', authorization);
     vi.stubEnv('INPUT_PATH', 'non-existent-path');
