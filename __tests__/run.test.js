29433fd440187b2ae5d79d0de2940d71bb6526dd
Kanad Gupta <8854718+kanadgupta@users.noreply.github.com>
2023-09-22 17:38:00 -0500

    chore: test suite upgrades (#58)
    * refactor: string constants
    
    * refactor: use MSW over nock
    
    * ci: test node 20
    
    * test: swap out jest for vitest
    
    and also upgrade the eslint config + friends to the latest

--- __tests__/run.test.js
+++ __tests__/run.test.js
@@ -1,11 +1,44 @@
-const nock = require('nock');
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, it, vi } from 'vitest';
 
-const run = require('../run');
+import run from '../run';
 
-const glitchNock = () =>
-  nock('https://api.glitch.com', { encodedQueryParams: true, reqheaders: { authorization: 'test-auth' } }).post(
-    '/project/githubImport'
-  );
+const glitchUrl = 'https://api.glitch.com/project/githubImport';
+
+const authorization = 'test-auth';
+const projectId = 'test-project-id';
+const repo = 'owner/repo';
+// optional param
+const path = 'test-path';
+
+/** Validates the query and header params of the Glitch API request */
+function validateReq(req, opts = {}) {
+  if (req.headers.get('authorization') !== authorization) {
+    throw new Error(`Mock error: expected ${authorization}, received ${req.headers.get('authorization')}`);
+  }
+  if (req.url.searchParams.get('projectId') !== projectId) {
+    throw new Error(`Mock error: expected ${projectId}, received ${req.url.searchParams.get('projectId')}`);
+  }
+
+  const currentPath = opts.path || null;
+  if (req.url.searchParams.get('path') !== currentPath) {
+    throw new Error(`Mock error: expected ${currentPath}, received ${req.url.searchParams.get('path')}`);
+  }
+
+  const currentRepo = opts.repo || repo;
+  if (req.url.searchParams.get('repo') !== currentRepo) {
+    throw new Error(`Mock error: expected ${currentRepo}, received ${req.url.searchParams.get('repo')}`);
+  }
+}
+
+const server = setupServer(
+  // Describe the requests to mock.
+  rest.post(glitchUrl, (req, res, ctx) => {
+    validateReq(req);
+    return res(ctx.status(200));
+  }),
+);
 
 describe('glitch-sync main runner tests', () => {
   let mockStdOut;
@@ -14,122 +47,123 @@ describe('glitch-sync main runner tests', () => {
     return [mockStdOut.mock.calls.join('\n\n')].filter(Boolean).join('\n\n');
   };
 
-  beforeEach(() => {
-    mockStdOut = jest.spyOn(process.stdout, 'write').mockImplementation();
+  beforeAll(() => {
+    server.listen({ onUnhandledRequest: 'error' });
+  });
 
-    process.env.GITHUB_REPOSITORY = 'owner/repo';
+  beforeEach(() => {
+    mockStdOut = vi.spyOn(process.stdout, 'write').mockImplementation(() => {});
+    vi.stubEnv('GITHUB_REPOSITORY', repo);
   });
 
   afterEach(() => {
-    delete process.env.GITHUB_REPOSITORY;
-    delete process.env['INPUT_AUTH-TOKEN'];
-    delete process.env['INPUT_PROJECT-ID'];
-    delete process.env.INPUT_PATH;
-
+    vi.unstubAllEnvs();
     mockStdOut.mockReset();
+    server.resetHandlers();
+  });
 
-    nock.cleanAll();
+  afterAll(() => {
+    server.close();
   });
 
   it('should fail if missing project ID param', async () => {
-    process.env['INPUT_AUTH-TOKEN'] = 'test-auth';
-
-    const scope = glitchNock().reply(200);
+    vi.stubEnv('INPUT_AUTH-TOKEN', authorization);
 
     await expect(run()).resolves.toBeUndefined();
 
     const output = getCommandOutput();
     expect(output).toContain('::error::Error syncing to Glitch: Input required and not supplied: project-id');
-    // assertion that nock is not called
-    expect(scope.isDone()).toBe(false);
   });
 
   it('should fail if missing auth param', async () => {
-    process.env['INPUT_PROJECT-ID'] = 'test-project-id';
-
-    const scope = glitchNock().reply(200);
+    vi.stubEnv('INPUT_PROJECT-ID', projectId);
 
     await expect(run()).resolves.toBeUndefined();
 
     const output = getCommandOutput();
     expect(output).toContain('::error::Error syncing to Glitch: Input required and not supplied: auth-token');
-    // assertion that nock is not called
-    expect(scope.isDone()).toBe(false);
   });
 
   it('should fail if Glitch API fails with empty response body', async () => {
-    process.env['INPUT_AUTH-TOKEN'] = 'test-auth';
-    process.env['INPUT_PROJECT-ID'] = 'test-project-id';
+    vi.stubEnv('INPUT_AUTH-TOKEN', authorization);
+    vi.stubEnv('INPUT_PROJECT-ID', projectId);
 
-    const scope = glitchNock().query({ projectId: 'test-project-id', repo: 'owner/repo' }).reply(403);
+    server.use(
+      rest.post(glitchUrl, (req, res, ctx) => {
+        validateReq(req);
+        return res(ctx.status(403));
+      }),
+    );
 
     await expect(run()).resolves.toBeUndefined();
 
     const output = getCommandOutput();
-    expect(output).toContain('::error::Error syncing to Glitch: null');
-    scope.done();
+    expect(output).toContain('::error::Error syncing to Glitch: Forbidden');
   });
 
   // TODO: is this even a response body that the Glitch API returns?
   // The error handling is a bit of a mess, that should be cleaned up at some point
   it('should fail if Glitch API fails with JSON response body', async () => {
-    process.env['INPUT_AUTH-TOKEN'] = 'test-auth';
-    process.env['INPUT_PROJECT-ID'] = 'test-project-id';
+    vi.stubEnv('INPUT_AUTH-TOKEN', authorization);
+    vi.stubEnv('INPUT_PROJECT-ID', projectId);
 
-    const scope = glitchNock()
-      .query({ projectId: 'test-project-id', repo: 'owner/repo' })
-      .reply(400, { stderr: 'yikes' });
+    server.use(
+      rest.post(glitchUrl, (req, res, ctx) => {
+        validateReq(req);
+        return res(ctx.status(400), ctx.json({ stderr: 'yikes' }));
+      }),
+    );
 
     await expect(run()).resolves.toBeUndefined();
 
     const output = getCommandOutput();
     expect(output).toContain('::error::Error syncing to Glitch: yikes');
-    scope.done();
   });
 
   it('should run with required parameters', async () => {
-    process.env['INPUT_AUTH-TOKEN'] = 'test-auth';
-    process.env['INPUT_PROJECT-ID'] = 'test-project-id';
-
-    const scope = glitchNock().query({ projectId: 'test-project-id', repo: 'owner/repo' }).reply(200);
+    vi.stubEnv('INPUT_AUTH-TOKEN', authorization);
+    vi.stubEnv('INPUT_PROJECT-ID', projectId);
 
     await expect(run()).resolves.toBeUndefined();
 
     const output = getCommandOutput();
     expect(output).toContain('Glitch project successfully updated! 🎉');
-    scope.done();
   });
 
   it('should run with optional path param', async () => {
-    process.env['INPUT_AUTH-TOKEN'] = 'test-auth';
-    process.env.INPUT_PATH = 'test-path';
-    process.env['INPUT_PROJECT-ID'] = 'test-project-id';
+    vi.stubEnv('INPUT_AUTH-TOKEN', authorization);
+    vi.stubEnv('INPUT_PATH', path);
+    vi.stubEnv('INPUT_PROJECT-ID', projectId);
 
-    const scope = glitchNock()
-      .query({ path: 'test-path', projectId: 'test-project-id', repo: 'owner/repo' })
-      .reply(200);
+    server.use(
+      rest.post(glitchUrl, (req, res, ctx) => {
+        validateReq(req, { path });
+        return res(ctx.status(200));
+      }),
+    );
 
     await expect(run()).resolves.toBeUndefined();
 
     const output = getCommandOutput();
     expect(output).toContain('Glitch project successfully updated! 🎉');
-    scope.done();
   });
 
   it('should run with optional repo param', async () => {
-    process.env['INPUT_AUTH-TOKEN'] = 'test-auth';
-    process.env.INPUT_PATH = 'test-path';
-    process.env['INPUT_PROJECT-ID'] = 'test-project-id';
-    process.env.INPUT_REPO = 'octocat/Hello-World';
+    vi.stubEnv('INPUT_AUTH-TOKEN', authorization);
+    vi.stubEnv('INPUT_PATH', path);
+    vi.stubEnv('INPUT_PROJECT-ID', projectId);
+    vi.stubEnv('INPUT_REPO', 'octocat/Hello-World');
 
-    const scope = glitchNock()
-      .query({ path: 'test-path', projectId: 'test-project-id', repo: 'octocat/Hello-World' })
-      .reply(200);
+    server.use(
+      rest.post(glitchUrl, (req, res, ctx) => {
+        validateReq(req, { path, repo: 'octocat/Hello-World' });
+        return res(ctx.status(200));
+      }),
+    );
 
     await expect(run()).resolves.toBeUndefined();
 
     const output = getCommandOutput();
     expect(output).toContain('Glitch project successfully updated! 🎉');
-    scope.done();
   });
 });
